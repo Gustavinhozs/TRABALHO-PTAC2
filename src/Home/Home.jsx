bc7c57f953acc4bc951a208862fb2a4ff382a4eb
Gustavin <131817115+Gustavinhozs@users.noreply.github.com>
2023-09-21 20:20:01 -0300

    gg
    gg

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-export default function Home() {
-   
-    return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/todo">todo</Link>
-        </div>
 
-    );
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Página Inicial</h1>
+      <Link to="/todo" className="todo-link">
+        Todo
+      </Link>
+    </div>
+  );
 }
