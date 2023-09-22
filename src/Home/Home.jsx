71d798b65721420b5f1e0c9939cebcd75b23f288
Gustavin <131817115+Gustavinhozs@users.noreply.github.com>
2023-09-22 03:14:59 -0300

    gg
    gg

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./home.css";
 
 export default function Home() {
   return (
-    
     <div className="home-container">
-      <h1 className="home-title">Página Inicial</h1>
-      <a href='http://localhost:5173/todo'><button>Lista de Joias preciosas</button></a>
+      <img src="https://cdn.discordapp.com/attachments/887389770681032735/1154657551858806835/Banner_promocao_halloween_preto_branco_e_roxo.png" alt="Banner" />
+      <h1 className="home-title">Descubra a Beleza das Pedras Preciosas</h1>
+      <h2>Explore a coleção mais deslumbrante de pedras preciosas do mundo.</h2>
+      <a href='http://localhost:5173/todo'><button>Saiba mais</button></a>
     </div>
   );
 }
