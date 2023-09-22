dc41f07a10d5b71d83b660d34219684cc499462f
Gustavin <131817115+Gustavinhozs@users.noreply.github.com>
2023-09-21 23:43:31 -0300

    gg
    gg

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
+    
     <div className="home-container">
       <h1 className="home-title">Página Inicial</h1>
-      <Link to="/todo" className="todo-link">
-        Todo
-      </Link>
+      <a href='http://localhost:5173/todo'><button>Lista de Joias preciosas</button></a>
     </div>
   );
 }
--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -6,15 +6,18 @@ export default function ToDo() {
     const [atividade, setAtividade] = useState("");
     const [lista, setLista] = useState([]);
     const [id, setId] = useState(1);
+    const [valor, setValor] = useState("");
 
     const salvar = (e) => {
         e.preventDefault();
         setLista([...lista, {
             atividade: atividade,
-            id: id
+            id: id, 
+            valor: valor
         }]);
         setId(id + 1);
         setAtividade("");
+        setValor("");
     };
     const remover = (id) => {
         /*setLista(lista.filter((ativ) => (ativ.id !== id ? lista : null)));*/
@@ -27,19 +30,29 @@ export default function ToDo() {
         setLista(auxLista);
     }
     return (
-        <div class="container">
-            <Link to="/">home</Link>
-            <h1>Lista das Pedras mais valiosas do mundo</h1>
+        <div className="container">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/2023_Obsidian_logo.svg/800px-2023_Obsidian_logo.svg.png"></img>
+            <a href='http://localhost:5173'><button>Home</button></a>
+            <h1>Pedras preciosas mais valiosas do mundo.</h1>
             <form onSubmit={salvar}>
-                <input type="text"
+                <input
+                    type="text"
                     value={atividade}
-                    onChange={(e) => { setAtividade(e.target.value) }} />
+                    onChange={(e) => setAtividade(e.target.value)}
+                    placeholder="Nome da Joia" /// subtítulo 
+                />
+                <input
+                    type="text"
+                    value={valor}
+                    onChange={(e) => setValor(e.target.value)}
+                    placeholder="Valor por grama" /// subtítulo 
+                />
                 <button>ADICIONAR</button>
             </form>
             {lista.map((ativ) =>
                 <ul key={ativ.id}>
                     <li>
-                        <p>{ativ.atividade}</p>
+                        <p>{ativ.id} {ativ.atividade} {ativ.valor} </p> 
                         <button onClick={() => remover(ativ.id)}>Remover</button>
                     </li>
                 </ul>
